0244756759eb3e15cae97c44603a57c35895a8da
iulloa33 <46978592+iulloa33@users.noreply.github.com>
2022-01-11 16:07:38 -0800

    Add files via upload

---
+++ Release/test.d
@@ -0,0 +1,13 @@
+# FIXED
+
+test.obj: ../test.c
+test.obj: ../Adafruit_GFX.h
+test.obj: ../Adafruit_SSD1351.h
+test.obj: ../glcdfont.h
+test.obj: ../test.h
+
+../test.c: 
+../Adafruit_GFX.h: 
+../Adafruit_SSD1351.h: 
+../glcdfont.h: 
+../test.h: 
